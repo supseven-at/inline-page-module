ba1accfc47a0f42a4e6c8f0d1dc3e0ff5fa954c5
Georg Großberger <g.grossberger@supseven.at>
2024-02-09 13:30:09 +0100

    [TASK] Add nonce to script tag

--- Classes/Listeners/PageLayoutContentModifierListener.php
+++ Classes/Listeners/PageLayoutContentModifierListener.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Supseven\InlinePageModule\Listeners;
 
 use TYPO3\CMS\Backend\Controller\Event\ModifyPageLayoutContentEvent;
+use TYPO3\CMS\Core\Domain\ConsumableString;
 
 /**
  * Modify the variables of the
@@ -15,8 +16,15 @@ class PageLayoutContentModifierListener
 {
     public function __invoke(ModifyPageLayoutContentEvent $event): void
     {
+        $nonceAttr = '';
+        $nonceAttribute = $event->getRequest()->getAttribute('nonce');
+
+        if ($nonceAttribute instanceof ConsumableString) {
+            $nonceAttr = ' nonce="' . htmlspecialchars($nonceAttribute->consume()) . '"';
+        }
+
         $event->addHeaderContent('
-        <script>
+        <script' . $nonceAttr . '>
             window.addEventListener("DOMContentLoaded", () => {
                 document.querySelectorAll(".t3-page-column-header .t3-page-column-header-icons").forEach(el => {
                     el.parentNode.removeChild(el);
